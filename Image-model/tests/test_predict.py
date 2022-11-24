879a3480e539fd867d19c77a7fd6237b0ada0214
serenaydc <114987513+serenaydc@users.noreply.github.com>
2022-11-24 09:57:36 +0100

    modified test

--- Image-model/tests/test_predict.py
+++ Image-model/tests/test_predict.py
@@ -1,45 +1,26 @@
 import json
 from PIL import Image
-import boto3
-from botocore import UNSIGNED  # contact public s3 buckets anonymously
-from botocore.client import Config  # contact public s3 buckets anonymously
-from io import BytesIO
+import joblib
+import os
+import sys
 
-from src.resnet_model import ResnetModel
-
-
-def load_files_from_s3(
-        keys,
-        bucket_name='bird-classification-bucket'
-        ):
-    """Retrieves files anonymously from my public S3 bucket"""
-    s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
-    s3_files = []
-    for key in keys:
-        s3_file_raw = s3.get_object(Bucket=bucket_name, Key=key)
-        s3_file_cleaned = s3_file_raw['Body'].read()
-        s3_file_image = Image.open(BytesIO(s3_file_cleaned))
-        s3_files.append(s3_file_image)
-    return s3_files
-
-
-def test_s3_connection():
-    s3_files = load_files_from_s3(["train/AFRICAN CROWNED CRANE/001.jpg"])
-    assert s3_files
-
+sys.path.insert(0, 'C:/Users/sdoga/Desktop/Courses/Software-Engineering/Image_model2/src')
+from Project import predict_proba
 
 def test_predict():
-    model = ResnetModel(
-        path_to_pretrained_model='models/trained_model_resnet50.pt')
+    model=joblib.load("models/trained_model.joblib")
     with open('src/index_to_class_label.json', 'rb') as f:
         index_to_class_labels = json.load(f)
     index_to_class_labels = {
         int(k): v for k, v in index_to_class_labels.items()}
-    img = load_files_from_s3(["train/AFRICAN CROWNED CRANE/001.jpg"])[0]
-    formatted_prediction = model.predict_proba(
+    img = Image.open("Datasets/BIRDS450/images/train/AFRICAN CROWNED CRANE/001.jpg")
+    formatted_prediction = predict_proba(
         img,
         3,
         index_to_class_labels,
-        show=False
+        model
         )
     assert formatted_prediction[0][0] == "African Crowned Crane"
+
+test_predict()
+
