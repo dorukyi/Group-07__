d4934a0b9115b0b19388a1f389970a3add69da33
Bas Verkennis <93135419+basverkennis@users.noreply.github.com>
2022-11-25 04:30:10 +0100

    Update unittest_imageclassification.py

--- Image-model/unit-test-image/unittest_imageclassification.py
+++ Image-model/unit-test-image/unittest_imageclassification.py
@@ -18,21 +18,3 @@ def testImageShape(image):
   assertion_message = "Image was not in right shape for the model. Shape should be 224x224."
   if not image.size == (224,224):
     raise AssertionError(assertion_message)
-
-# Example correct case
-from PIL import Image
-path = '/content/drive/MyDrive/SE project/Data/Image Dasta/Birds images/test/AFRICAN CROWNED CRANE/1.jpg'
-img_file = Image.open(path)
-image = img_file.resize((224,224))
-testImageExtension(path)
-testImageShape(image)
-
-# Example incorrect case - first unit test
-from PIL import Image
-path = '/content/drive/MyDrive/SE project/Notebooks/Classification/Image/incorrect_unittestcase_exampleimage.png'
-img_file = Image.open(path)
-image = img_file.resize((200,200))
-testImageExtension(path)
-
-# Example incorrect case - second unit test
-testImageShape(image)
