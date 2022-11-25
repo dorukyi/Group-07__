ddbb2baa03a934aa24defb94f435b9ab1b9aa671
Bas Verkennis <93135419+basverkennis@users.noreply.github.com>
2022-11-25 04:34:57 +0100

    Update unit-test-cases-image.py

--- Image-model/unit-test-image/unit-test-cases-image.py
+++ Image-model/unit-test-image/unit-test-cases-image.py
@@ -3,7 +3,7 @@ from unittest_imageclassification import testImageExtension, testImageShape
 
 # Example correct case
 from PIL import Image
-path = '/content/drive/MyDrive/SE project/Data/Image Dasta/Birds images/test/AFRICAN CROWNED CRANE/1.jpg'
+path = '1.jpg'
 img_file = Image.open(path)
 image = img_file.resize((224,224))
 testImageExtension(path)
@@ -11,7 +11,7 @@ testImageShape(image)
 
 # Example incorrect case - first unit test
 from PIL import Image
-path = '/content/drive/MyDrive/SE project/Notebooks/Classification/Image/incorrect_unittestcase_exampleimage.png'
+path = 'incorrect_unittestcase_exampleimage.png'
 img_file = Image.open(path)
 image = img_file.resize((200,200))
 testImageExtension(path)
