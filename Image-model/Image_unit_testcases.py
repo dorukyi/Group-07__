42a231a2388b3801723803de3b3f9004c8d62b72
Bas Verkennis <93135419+basverkennis@users.noreply.github.com>
2022-11-25 04:28:26 +0100

    Update Image_unit_testcases.py

--- Image-model/Image_unit_testcases.py
+++ Image-model/Image_unit_testcases.py
@@ -10,7 +10,7 @@ testImageShape(image)
 
 # Example incorrect case - first unit test
 from PIL import Image
-path = '/content/drive/MyDrive/SE project/Notebooks/Classification/Image/incorrect_unittestcase_exampleimage (1).png'
+path = '/content/drive/MyDrive/SE project/Notebooks/Classification/Image/incorrect_unittestcase_exampleimage.png'
 img_file = Image.open(path)
 image = img_file.resize((200,200))
 testImageExtension(path)
