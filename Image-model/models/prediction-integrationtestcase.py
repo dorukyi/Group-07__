d8b3fa9f90904f63cde7fa097ef6709f13fec0f1
basverkennis <b.verkennis.consultancy@gmail.com>
2022-11-26 23:58:19 +0100

    adding the model4 with terminal

--- Image-model/models/prediction-integrationtestcase.py
+++
@@ -1,6 +0,0 @@
-from prediction-integrationtestcase import image_reshaping, predict_birdImage
-
-# Example - AFRICAN CROWNED CRANE - class 3
-image_path = '1.jpg'
-img, img_file = image_reshaping(image_path)
-predict_birdImage(img, img_file)
--- README.md
+++
@@ -1,3 +0,0 @@
-# Group-07__
-
-The repository of Group 07__ for the Software Engineering for CSAI Group Project.
